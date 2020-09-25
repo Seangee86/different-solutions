cfe33c9ca254f5eac4f5269a1a54ee7c367f97bb
Sean Granier <seanlg624@gmail.com>
2020-09-25 08:33:46 -0500

    update

---
+++ mysolution.py
@@ -0,0 +1,4 @@
+unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
+#sorted_list = [('second_element', 2), ('first_element', 4), ('third_element', 6)]
+
+# havnt learned about for loops and if statments
